56063028abd60f95d5ae2fb4ad5b3406a185707c
Casper Boone <mail@casperboone.nl>
2016-08-25 12:02:24 +0000

    Applied fixes from StyleCI

--- src/PushoverChannel.php
+++ src/PushoverChannel.php
@@ -55,7 +55,7 @@ class PushoverChannel
         } catch (ServiceCommunicationError $serviceCommunicationError) {
             $this->events->fire(
                 new NotificationFailed($notifiable, $notification, 'pushover', [
-                    $serviceCommunicationError->getMessage()
+                    $serviceCommunicationError->getMessage(),
                 ])
             );
         }
